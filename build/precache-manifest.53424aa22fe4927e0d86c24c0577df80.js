a3422d0071a34b47e1a3ce611f110392fc4fee25
GonzaVega <gonza.vega@icloud.com>
2022-10-14 11:09:26 -0300

    app build to production, hosted in Firebase

---
+++ build/precache-manifest.53424aa22fe4927e0d86c24c0577df80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a722df742f4a10e73ccf1e7a15a28e48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "feb32c8f50f8caff347e",
+    "url": "/static/css/main.2cd32184.chunk.css"
+  },
+  {
+    "revision": "e0ca9b89040ec63aee37",
+    "url": "/static/js/2.102389e8.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.102389e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feb32c8f50f8caff347e",
+    "url": "/static/js/main.5dafbaf1.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.5dafbaf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4054bc057732f69f878d",
+    "url": "/static/js/runtime-main.13ba5424.js"
+  }
+]);
